fb891f353f1a1a4d42764ccc93ca3a7fc8c96285
Radek Slupik <rightfold@gmail.com>
2015-07-25 19:21:37 +0200

    Remove AppleScript in favour of the open command

--- lib/Network/Browser/OSX.hs
+++ lib/Network/Browser/OSX.hs
@@ -5,15 +5,8 @@ module Network.Browser.OSX
 import System.Exit (ExitCode(..))
 import System.Process (rawSystem)
 
-osxCommand :: String -> (String, [String])
-osxCommand url = ("/usr/bin/osascript", argv)
-    where argv = ["-e", appleScript, url]
-          appleScript = "on run argv\n\
-                        \    open location (item 1 of argv)\n\
-                        \end run\n"
-
 openBrowserOSX :: String -> IO Bool
 openBrowserOSX url = exitCodeToBool `fmap` rawSystem executable argv
-    where (executable, argv) = osxCommand url
+    where (executable, argv) = ("open", [url])
           exitCodeToBool ExitSuccess     = True
           exitCodeToBool (ExitFailure _) = False
