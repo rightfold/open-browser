96182d1ffd6f6de0ba41bf874030aa86782d588a
Radek Slupik <rightfold@gmail.com>
2015-07-27 09:38:41 +0200

    Add support for Linux

--- lib/Network/Browser.hs
+++ lib/Network/Browser.hs
@@ -2,9 +2,15 @@ module Network.Browser
 ( openBrowser
 ) where
 
+import Data.List (isInfixOf)
+import Network.Browser.Linux (openBrowserLinux)
 import Network.Browser.OSX (openBrowserOSX)
+import System.Info (os)
 
 -- |'openBrowser' opens a URL in the user's preferred web browser. Returns
 -- whether or not the operation succeeded.
 openBrowser :: String -> IO Bool
-openBrowser = openBrowserOSX
+openBrowser
+    | "linux"  `isInfixOf` os = openBrowserLinux
+    | "darwin" `isInfixOf` os = openBrowserOSX
+    | otherwise               = error "unsupported platform"
---
+++ lib/Network/Browser/Linux.hs
@@ -0,0 +1,12 @@
+module Network.Browser.Linux
+( openBrowserLinux
+) where
+
+import System.Exit (ExitCode(..))
+import System.Process (rawSystem)
+
+openBrowserLinux :: String -> IO Bool
+openBrowserLinux url = exitCodeToBool `fmap` rawSystem executable argv
+    where (executable, argv) = ("xdg-open", [url])
+          exitCodeToBool ExitSuccess     = True
+          exitCodeToBool (ExitFailure _) = False
