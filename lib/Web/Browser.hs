52767980ea0bae2f1b8853e785bd3c26249fa30e
RyanGlScott <ryan.gl.scott@ku.edu>
2015-07-27 11:55:14 -0500

    Add Windows and *BSD support

--- lib/Web/Browser.hs
+++ lib/Web/Browser.hs
@@ -6,11 +6,14 @@ import Data.List (isInfixOf)
 import System.Info (os)
 import Web.Browser.Linux (openBrowserLinux)
 import Web.Browser.OSX (openBrowserOSX)
+import Web.Browser.Windows (openBrowserWindows)
 
 -- |'openBrowser' opens a URL in the user's preferred web browser. Returns
 -- whether or not the operation succeeded.
 openBrowser :: String -> IO Bool
 openBrowser
-    | "linux"  `isInfixOf` os = openBrowserLinux
-    | "darwin" `isInfixOf` os = openBrowserOSX
-    | otherwise               = error "unsupported platform"
+    | "linux"   `isInfixOf` os = openBrowserLinux
+    | "darwin"  `isInfixOf` os = openBrowserOSX
+    | "mingw32" `isInfixOf` os = openBrowserWindows
+    | "bsd"     `isInfixOf` os = openBrowserLinux
+    | otherwise                = error "unsupported platform"
---
+++ lib/Web/Browser/Windows.hs
@@ -0,0 +1,21 @@
+﻿module Web.Browser.Windows
+( openBrowserWindows
+) where
+
+import System.Directory (findExecutable)
+import System.Exit (ExitCode(..))
+import System.Process (system)
+
+openBrowserWindows :: String -> IO Bool
+openBrowserWindows url = do
+  mbCygstart <- findExecutable "cygstart"
+  case mbCygstart of
+       Just cygstart -> openBrowserWith cygstart url
+       Nothing       -> openBrowserWith "start" url
+
+-- We can't use rawSystem on Windows, since that doesn't pick up the "start"
+-- shell built-in.
+openBrowserWith :: FilePath -> String -> IO Bool
+openBrowserWith cmd url = exitCodeToBool `fmap` system (cmd ++ " " ++ url)
+    where exitCodeToBool ExitSuccess     = True
+          exitCodeToBool (ExitFailure _) = False
