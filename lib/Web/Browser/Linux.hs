2f5714b144ae071747481fd1ce7f4074b737e955
Radek Slupik <rightfold@gmail.com>
2015-07-30 11:31:46 +0200

    Silence xdg-open
    Fixes #7

--- lib/Web/Browser/Linux.hs
+++ lib/Web/Browser/Linux.hs
@@ -7,6 +7,6 @@ import System.Process (rawSystem)
 
 openBrowserLinux :: String -> IO Bool
 openBrowserLinux url = exitCodeToBool `fmap` rawSystem executable argv
-    where (executable, argv) = ("xdg-open", [url])
+    where (executable, argv) = ("sh", ["-c", "xdg-open \"$0\" 2>&1 > /dev/null", url])
           exitCodeToBool ExitSuccess     = True
           exitCodeToBool (ExitFailure _) = False
